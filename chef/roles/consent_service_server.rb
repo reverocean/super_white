2b9d135efa07c8f51cb59c5edef08d97101b8886
Junfeng Dai <ryandjf@hotmail.com>
2013-08-28 08:15:54 +0800

    add java attributes in role

--- chef/roles/consent_service_server.rb
+++ chef/roles/consent_service_server.rb
@@ -1,5 +1,15 @@
 name 'consent_service_server'
 description 'A single consent service server'
+
+default_attributes(
+  :java => {
+  	 :install_flavor => oracle,
+     :oracle => {
+       "accept_oracle_download_terms" => true
+     }
+   }
+)
+
 run_list(
   'recipe[java]',
   'recipe[consent_service]',
