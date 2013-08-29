ac5e8875b057e2f4d2cc4902fc3aff9375a7ed38
Junfeng Dai <ryandjf@hotmail.com>
2013-08-29 16:07:03 +0800

    add more AWS options

--- chef/.chef/knife.rb
+++ chef/.chef/knife.rb
@@ -17,4 +17,7 @@ cookbook_path            ["#{current_dir}/../cookbooks"]
  knife[:aws_access_key_id] = "#{ENV['AWS_ACCESS_KEY_ID']}"
  knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
  knife[:aws_ssh_key_id] = "#{ENV['AWS_KEY_ID']}"
+ knife[:availability-zone] = "us-east-1a"
+ knife[:flavor] = "t1.micro"
+ knife[:ssh-user] = "ubuntu"
  
